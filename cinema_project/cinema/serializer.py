fc9269f773a880054bb22816c0744b5ef69d59ad
Tymur <mr.kanis82@gmail.com>
2026-09-16 18:03:32 -0400

    add serializer.py with MovieSerializer based oт rest framework.

---
+++ cinema_project/cinema/serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Movie
+
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.duration = validated_data.get("duration", instance.duration)
+        instance.save()
+        return instance
